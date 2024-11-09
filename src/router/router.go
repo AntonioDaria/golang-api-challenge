0a34f00afb76e37eca953a5e00c98e00adb08dd8
Antonio D'aria <antoniodaria@msn.com>
2024-11-09 12:29:47 +0100

    added graceful shutdown and panic middleware

--- src/router/router.go
+++ src/router/router.go
@@ -3,10 +3,17 @@ package router
 import (
 	"github.com/AntonioDaria/surfe/src/handlers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func New() *fiber.App {
 	router := fiber.New()
+
+	// Add Recover middleware to handle panics
+	router.Use(recover.New(recover.Config{
+		EnableStackTrace: true,
+	}))
+
 	router.Get("/health", handlers.HealthCheckHandler) // Register health check endpoint
 	return router
 }
